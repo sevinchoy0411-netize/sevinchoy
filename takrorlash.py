c16f4ce45f81a35d1acb5ebe3c70560dbf434774
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2025-12-09 17:56:39 +0500

    Create takrorlash.py

---
+++ takrorlash.py
@@ -0,0 +1,54 @@
+# TUPLE
+# sonlar = (10,20,30,40)
+# print(sonlar , type(sonlar))
+
+# DICT
+# eng_uzb = {
+#     "aplle" : "olma",
+#     "cherry" : "olcha",
+#     "pen" : "ruchka",
+#     "pencil" : "qalam"
+# }
+# print(eng_uzb.keys(),eng_uzb.values(),eng_uzb.items())
+# soz_tarjima = str(input("soz kiriting: "))
+# print(eng_uzb.get(soz_tarjima,"unday so'z yo'q"))
+
+# BREAK
+# for i in range(1,150):
+#     if i ==  100:
+#         break
+#     print(i)
+
+# CONTINUE
+# for i in range(1,150):
+#     if i == 100:
+#         continue
+#     print(i)
+
+# PASS
+# def yigindi(a,b):
+#     pass
+# print(yigindi(10,20))
+
+# programming = {
+#     'programmer' : "dasturchi",
+#     'developer' : "dasturchi",
+#     'syntax' : "kod yozish qoidasi",
+#     'algorithm' : "algoritm",
+#     'variable' : "o'zgaruvchi",
+#     'function' : "funksiya",
+#     'data' : "ma'lumot",
+#     'erroe' : "xato",
+#     'memory' : "xotira",
+#     'module' : "modul",
+#     'network' : "tarmoq",
+#     'user' : "foydalanuvchi",
+#     'backend' : "ma'lumotlar bazasi bilan shug'ullanadigan qism",
+#     'frontend' : "interfeys",
+#     'object' : "obyekt",
+#     'comment' : "izoh",
+#     'library' : "kutubxona",
+#     'string' : "matn",
+#     'boolean' : "mantiqiy qiymat",
+#     'inheritance' : "merosho'rlik"
+# }
