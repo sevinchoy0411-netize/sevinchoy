8c411df2c43bcf441a27bbb3f207906ef62dddf0
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2025-12-16 16:40:31 +0500

    .

--- takrorlash.py
+++ takrorlash.py
@@ -30,6 +30,7 @@
 #     pass
 # print(yigindi(10,20))
 
+# DICT
 # programming = {
 #     'programmer' : "dasturchi",
 #     'developer' : "dasturchi",
@@ -50,5 +51,63 @@
 #     'library' : "kutubxona",
 #     'string' : "matn",
 #     'boolean' : "mantiqiy qiymat",
-#     'inheritance' : "merosho'rlik"
+#     'inheritance' : "merosxo'rlik"
 # }
+
+
+# LAMBDA
+# x = lambda a,b: max(a,b)
+# print(x(10,20))
+
+# x = lambda a,b,c: f"{a} katta" if a>b and a>c else (f"{b} katta" if b>a and b>c else ("teng" if a==b and a==c else f"{c} katta"))
+# print(x(50,50,50))
+
+
+# REKURSIYA
+# def raqam_soni(son):
+#     if son == 0:
+#         return 0
+#     else: 
+#         return 1 + raqam_soni(son//10)
+# print(raqam_soni(123456789))
+
+# from words import words
+# import random
+# tasodifiy_soz = random.choice(list(words))
+# print(tasodifiy_soz)
+
+# OS
+# import os
+# print(os.getcwd()) #cwd - current working directionary #joriy papkani bilish
+# print(os.listdir()) #papkadagi fayllar royhati
+# os.mkdir("Python tili") #yangi papka yaratish
+# os.rmdir("Python tili") #papkani o'chitish
+# os.makedirs("a/b/c") #papkalar yaratish
+# os.removedirs("a/b/c") #papkalar o'chirish
+# print(os.path.exists("sevinchoy")) #bor-yo'qlik
+# print(os.system()) #OS buyruq
+
+# f = open("sevinchoy/test.txt","r")
+# matn = f.read()
+# print(matn)
+# f.close()
+
+# f = open("sevinchoy/test.txt" "r")
+# for qator in f:
+#     print(qator)
+# f.close()
+
+# f = open("sevinchoy/test.txt","w")
+# matn = input("matn kiriting: ")
+# f.write(matn)
+# f.close()
+
+# a = open("sevinchoy/test.txt","r")
+# oqi = a.read()
+# print(oqi)
+
+# with open("test.txt","r") as matn:
+#     print(matn.read())
+
+# with open("test.txt","a") as a:
+#     a.write("\nYangi qator")
