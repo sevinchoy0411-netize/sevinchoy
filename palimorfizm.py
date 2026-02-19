2898cc06c75da23d98718b92df0f1d8b744bbd0d
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2026-02-19 18:10:57 +0500

    Update palimorfizm.py

--- palimorfizm.py
+++ palimorfizm.py
@@ -101,3 +101,91 @@
 # qurilmalar = [Kompyuter("hp"),Telefon("iPhone")]
 # for i in qurilmalar:
 #     print(i.ishlab_chiqaruvchi())
+
+
+# 6
+# class Hayvon:
+#     def harakat(self):
+#         return "Hayvonclar harakati"
+# class Qush(Hayvon):
+#     def harakat(self):
+#         return "Qush uchyabdi"
+# class Baliq(Hayvon):
+#     def harakat(self):
+#         return "Baliq suzmoqda"
+# hayvonlar = [Qush(),Baliq()]
+# for hayvon in hayvonlar:
+#     print(hayvon.harakat())
+
+
+# 7
+# class Avto:
+#     def narxi(self):
+#         return "Mashina narxlari"
+# class Malibu(Avto):
+#     def __init__(self,price):
+#         self.price = price
+#     def narxi(self):
+#         return f"Malibu narxi: {self.price}$"
+# class Cobalt(Avto):
+#     def __init__(self,price):
+#         self.price = price
+#     def narxi(self):
+#         return f"Cobalt narxi: {self.price}$"
+# class Nexia(Avto):
+#     def __init__(self,narx):
+#         self.price = narx
+#     def narxi(self):
+#         return f"Nexia narxi: {self.price}$"
+# avtolar = [Malibu(25000),Cobalt(14500),Nexia(10000)]
+# for i in avtolar:
+#     print(i.narxi())
+
+
+# 8
+# class Asbob:
+#     def ovoz_chiqar(self):
+#         return "Musiqa asboblari ovozi"
+# class Gitara(Asbob):
+#     def ovoz_chiqar(self):
+#         return "Ting-ting"
+# class Baraban(Asbob):
+#     def ovoz_chiqar(self):
+#         return "Tum-tum"
+# asboblar = [Gitara(),Baraban()]
+# for ovoz in asboblar:
+#     print(ovoz.ovoz_chiqar())
+
+
+# 9
+# class Oyinchi:
+#     def sport_turi(self):
+#         return "Sportchilarning sport turlari"
+# class Futbolchi(Oyinchi):
+#     def sport_turi(self):
+#         return "Sport turi: futbol"
+# class Basketbolchi(Oyinchi):
+#     def sport_turi(self):
+#         return "Sport turi: basketbol"
+# sport = [Futbolchi(),Basketbolchi()]
+# for sports in sport:
+#     print(sports.sport_turi())
+
+
+# 10
+# class Raqam:
+#     def aniqlik(self):
+#         return "Raqamlar"
+# class Juftson(Raqam):
+#     def aniqlik(self):
+#         for i in range(1,10):
+#             if i%2==0:
+#                 print(i)
+# class Toqson(Raqam):
+#     def aniqlik(self):
+#         for i in range(1,10):
+#             if i%2!=0:
+#                 print(i)
+# sonlar = [Juftson(),Toqson()]
+# for son in sonlar:
+#     print(son.aniqlik())
