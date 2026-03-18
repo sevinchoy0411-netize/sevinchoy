9ed1db99db2a1525ba7efbe045ad72fe3dfe3dab
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2026-03-18 15:41:00 +0500

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -1,11 +1,24 @@
-class Bank:
-    def __init__(self,id,name,age,money,password):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.__money = money
-        self._password = password
-    def get_info(self):
-        return f"Mijoz ID: {self.id}\nMijoz name: {self.name}\nMijoz age: {self.age}\nMijoz puli: {self.__money}\nMijoz paroli: {self._password}"
-mijoz = Bank(1,'rayhona',25,500000000,2011)
-print(mijoz.get_info())
+# class Bank:
+#     def __init__(self,id,name,age,money,password):
+#         self.id = id
+#         self.name = name
+#         self.age = age
+#         self.__money = money
+#         self._password = password
+#     def get_info(self):
+#         return f"Mijoz ID: {self.id}\nMijoz name: {self.name}\nMijoz age: {self.age}\nMijoz puli: {self.__money}\nMijoz paroli: {self._password}"
+# mijoz = Bank(1,'rayhona',25,500000000,2011)
+# print(mijoz.get_info())
+
+
+
+import json
+
+# data = {"Ism" : "Vali","Yoshi" : 20}
+
+# with open("data.json","w") as matn:
+#     json.dump(data,matn)
+
+# with open("data.json","r") as f:
+#     malumot = json.load(f)
+# print(malumot)
