e6865e2e2d4898069515e436180e08be82b73c8e
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2026-03-25 17:40:49 +0500

    .

--- preparation.py
+++ preparation.py
@@ -32,27 +32,16 @@ import csv
 #     writer.writerow(["Ism", "Yosh"])
 #     writer.writerow(["Ali", 20])
 
-# finally nima qiladi?
-# Custom exception nima?
-# raise operatori nima qiladi?
-# Exception turlariga misol keltiring.
-# try-except-else qanday ishlaydi?
-# append mode nima?
-# json.load va json.loads farqi?
-# json.dump va json.dumps farqi?
-# Python dictni JSONga qanday o'tkazamiz?
-# JSON string nima?
-# csv nima?
-# csv modulidan qanday foydalaniadi?
-# reader va writer farqi nimada?
-# delimiter nima?
 
 
 
-# Database yaratish
-# Databaseni ulanishini alohida modul qo'yib db.py chaqirish
-# jadval yaratish(id,name,age,email,phone)
-# jadvalga malumot qoshish
-# malumotni yangilash
-# malumotni ochirish
-# malumot olish
+# class Maktab:
+#     def __init__(self,id,name,clas):
+#         self.id = id
+#         self.name = name
+#         self.clas = clas
+#     def __str__(self):
+#         return f"Talaba ID'si: {self.id}\nTalaba ismi: {self.name}\nTalaba sinfi: {self.clas}"
+
+# talaba1 = Maktab(1,"Abdulla","7-B")
+# print(talaba1)
