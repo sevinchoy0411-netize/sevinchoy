9bc0d0c14264abd480e84c0c11d1d3de0e58d1f6
Sevinchoy_ITgirl <sevinchoy0411@gmail.com>
2026-03-26 18:02:58 +0500

    Update db.py

--- db.py
+++ db.py
@@ -8,21 +8,21 @@ con = psycopg2.connect(
 con.autocommit = True
 cur = con.cursor()
 
-cur.execute(
-    "INSERT INTO students (name,age,subject,sinf) VALUES('',14,'backend',8)"
-)
+# cur.execute(
+#     "INSERT INTO students (name,age,subject,sinf) VALUES('Sevinchoy',14,'backend',8)"
+# )
 
 # cur.execute(
-#     "SELECT * FROM users"
+#     "SELECT * FROM students"
 # )
 # rows = cur.fetchall()
 # for row in rows:
 #     print(row)
 
 # cur.execute(
-#     "UPDATE users SET age=age-1 WHERE id=4"
+#     "UPDATE students SET age=age-1 WHERE id=13"
 # )
 
 # cur.execute(
-#     "DELETE FROM users WHERE id=1"
+    # "DELETE FROM students WHERE id=1"
 # )
